9c9211d994f903cfc30bc8a323bd23ea08120748
Skyggebjerg <ole@skyggebjerg.dk>
2024-02-12 17:28:51 +0100

    Serial communication

--- src/main.cpp
+++ src/main.cpp
@@ -19,18 +19,18 @@ int step_divisition = 32; //32
 int en_pin = 22;
 int dir_pin = 23;
 int step_pin = 19;
-//bool ValveOK = LOW;
+unsigned long startTime;
+unsigned long elapsedTime;
 
 int step = 0;
 int speed = 0;
-
-//int toggler =0;
 
 StepperDriver ss(motor_steps, step_divisition, en_pin, dir_pin, step_pin);
 
 void setup()
 {
   M5.begin(true, false, true);
+  Serial.begin(115200);
   pinMode(32, INPUT_PULLDOWN); // set pin mode to input
   pinMode(26, OUTPUT);
   digitalWrite(26, LOW);
@@ -46,10 +46,13 @@ void loop()
     //for (int i = 1; i <= 30; i++) {
       while (1)
 
-      {      
+      {
+      startTime = millis();     
       ss.powerEnable(true);
       ss.setSpeed(600); // 60 = 60 revolutions per minute (rpm) = 1 rev per sec (There are 6.5 revs per 100 µl)
-      ss.step(-1300,100,100); //aspirate (1300 steps = 6.5 revs = 100 µl)
+      Serial.println("AspirateStart:-650");
+      ss.step(-650,100,100); //aspirate (1300 steps = 6.5 revs = 100 µl)
+      Serial.println("AspirateEnd:-650");
       delay(100);
       digitalWrite(26, HIGH); // Tell Valve to change
       
@@ -60,7 +63,9 @@ void loop()
       //ss.setSpeed(1); // 1 revs/min = 60 revs/h = 9.23 full syringe strokes (60/6.5) = 923 µl per hour (9.23 x 100µl)
       ss.setSpeed(15); // 14 revs/min = 12.922 µl/h (14 x 923 µl)
       //ss.setSpeed(3); // 3 revs/min = 2.77 ml/h (3 x 923 µl)   
-      ss.step(1300); //dispense
+      Serial.println("DispenseStart:650");
+      ss.step(650); //dispense
+      Serial.println("DispenseEnd:650");
       delay(100);
       digitalWrite(26, LOW); // Tell Valve to change back 
       ss.powerEnable(false);
@@ -69,6 +74,9 @@ void loop()
         break;
       }
       delay(1000);
+      elapsedTime = millis() - startTime;
+      Serial.print("Elapsed:");
+      Serial.println(elapsedTime);
    //  delay(500); // include in for loop
    //} // for loop end
    }
